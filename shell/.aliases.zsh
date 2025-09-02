33c34777d33a111a12e1aa4ab3c0af78d47d0555
sebacruz1 <sebastiancruzpomar@gmail.com>
2025-09-02 13:31:11 -0400

    cambios menores

--- shell/.aliases.zsh
+++ shell/.aliases.zsh
@@ -4,6 +4,4 @@ alias ..='cd ..'; alias ...='cd ../..'; alias ....='cd ../../..'
 alias ll='eza -l --git --icons'; alias la='eza -la --git --icons'
 alias python='python3'; alias pip='pip3'
 alias please='sudo $(fc -ln -1)'
-alias ic='cd ~/Library/Mobile\ Documents/com\~apple\~CloudDocs'
-alias jukebox='ssh sebadell@192.168.100.50'
 

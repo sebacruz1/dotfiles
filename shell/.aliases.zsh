d24bee4464edf41f50a0221c162e718b8e53d2f8
sebacruz1 <sebastiancruzpomar@gmail.com>
2025-09-02 13:19:41 -0400

    arreglo aliases locales

--- shell/.aliases.zsh
+++ shell/.aliases.zsh
@@ -4,6 +4,6 @@ alias ..='cd ..'; alias ...='cd ../..'; alias ....='cd ../../..'
 alias ll='eza -l --git --icons'; alias la='eza -la --git --icons'
 alias python='python3'; alias pip='pip3'
 alias please='sudo $(fc -ln -1)'
-alias jukebox='ssh jukebox mpv --hwdec=vaapi'
 alias ic='cd ~/Library/Mobile\ Documents/com\~apple\~CloudDocs'
+alias jukebox='ssh sebadell@192.168.100.50'
 

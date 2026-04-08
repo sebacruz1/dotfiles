5ddef8d708b2abed1783ce78ec948ba174a8e1bc
sebacruz1 <sebastiancruzpomar@gmail.com>
2026-04-08 19:53:56 -0400

    Resize y cambio de panes con smartplits

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -5,7 +5,6 @@ local map = vim.keymap.set
 -- básicos
 map("n", "<leader>w", "<cmd>w<CR>", { silent = true })
 
---
 local function smart_close_buffer()
 	local listed_buffers = vim.fn.getbufinfo({ buflisted = 1 })
 
@@ -17,22 +16,48 @@ local function smart_close_buffer()
 	end
 end
 
-vim.keymap.set("n", ",q", smart_close_buffer, { desc = "Cerrar buffer o ir al inicio", silent = true })
+map("n", ",q", smart_close_buffer, { desc = "Cerrar buffer o ir al inicio", silent = true })
 
-vim.keymap.set("n", "<leader>x", "<cmd>close<CR>")
-vim.keymap.set("n", "<localleader>q", "<cmd>qa<CR>")
+map("n", "<leader>x", "<cmd>close<CR>")
+map("n", "<localleader>q", "<cmd>qa<CR>")
 map("n", "<Esc>", "<cmd>nohlsearch<CR>", { silent = true })
 
-vim.keymap.set("n", "<leader>|", "<cmd>vsplit<CR>", { desc = "Split + Buscar Archivo" })
-vim.keymap.set("n", "<leader>-", "<cmd>split<CR>", { desc = "Split + Buscar Archivo" })
+map("n", "<leader>|", "<cmd>vsplit<CR>", { desc = "Split + Buscar Archivo" })
+map("n", "<leader>-", "<cmd>split<CR>", { desc = "Split + Buscar Archivo" })
 
+map("n", "<leader>p", '"+p', { desc = "Pegar desde el sistema" })
+map("i", "<C-v>", "<C-r>+", { desc = "Pegar desde el sistema en insertar" })
 
+map({ "n", "v" }, "<leader>y", '"+y', { desc = "Copiar al sistema" })
 
-vim.keymap.set("n", "<leader>p", '"+p', { desc = "Pegar desde el sistema" })
-vim.keymap.set("i", "<C-v>", "<C-r>+", { desc = "Pegar desde el sistema en insertar" })
+map("n", "q:", "<nop>")
+map("n", "q/", "<nop>")
+map("n", "q?", "<nop>")
 
-vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', { desc = "Copiar al sistema" })
+-- smart-splits: navegar panes
+map("n", "<C-h>", function()
+	require("smart-splits").move_cursor_left()
+end, { desc = "Move left" })
+map("n", "<C-j>", function()
+	require("smart-splits").move_cursor_down()
+end, { desc = "Move down" })
+map("n", "<C-k>", function()
+	require("smart-splits").move_cursor_up()
+end, { desc = "Move up" })
+map("n", "<C-l>", function()
+	require("smart-splits").move_cursor_right()
+end, { desc = "Move right" })
 
-vim.keymap.set("n", "q:", "<nop>")
-vim.keymap.set("n", "q/", "<nop>")
-vim.keymap.set("n", "q?", "<nop>")
+-- smart-splits: resize panes
+map("n", "<C-Left>", function()
+	require("smart-splits").resize_left()
+end, { desc = "Resize left" })
+map("n", "<C-Right>", function()
+	require("smart-splits").resize_right()
+end, { desc = "Resize right" })
+map("n", "<C-Up>", function()
+	require("smart-splits").resize_up()
+end, { desc = "Resize up" })
+map("n", "<C-Down>", function()
+	require("smart-splits").resize_down()
+end, { desc = "Resize down" })

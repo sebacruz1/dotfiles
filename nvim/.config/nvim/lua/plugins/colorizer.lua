1198190abc0d2cdaa9ca6ae1ab0432593355ba0e
sebacruz1 <sebastiancruzpomar@gmail.com>
2026-04-08 20:52:01 -0400

    Arreglo en ruta de kitty y plugins de nvim

--- nvim/.config/nvim/lua/plugins/colorizer.lua
+++ nvim/.config/nvim/lua/plugins/colorizer.lua
@@ -1,19 +1,16 @@
 return {
 	{
-		"norcalli/nvim-colorizer.lua",
+		"catgoose/nvim-colorizer.lua",
 		event = { "BufReadPost", "BufNewFile" },
 		config = function()
-			require("colorizer").setup(
-				{ "*" },
-				{
-					RGB = true,
-					RRGGBB = true,
-					names = false,
-					tailwind = true,
-					rgb_fn = true,
-					mode = "background",
-				}
-			)
+			require("colorizer").setup({ "*" }, {
+				RGB = true,
+				RRGGBB = true,
+				names = false,
+				tailwind = true,
+				rgb_fn = true,
+				mode = "background",
+			})
 		end,
 	},
 }
